a54c7413b9ac9b146337d3286484c4d4e98ea481
dm13y <5132095@tut.by>
2017-10-23 00:04:07 +0300

    a sketch of the solution HW_02

---
+++ HW_02/src/main/java/by/dm13y/study/ObjectSizeCalc.java
@@ -0,0 +1,186 @@
+package by.dm13y.study;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ObjectSizeCalc {
+    private static final BitJRE curArch;
+    private static final String total_size_hdr = "total_size:";
+    private static final String shift_size_hdr = "shift_size";
+
+    static {
+        curArch = System.getProperties().getProperty("os.arch").contains("64") ? BitJRE.x64 : BitJRE.x32;
+    }
+
+    public static int getPrimitiveSizeByName(String typeName){
+        switch (typeName){
+            case "boolean": return 1;
+            case "byte" :return 1;
+            case "short" :return 2;
+            case "char": return 2;
+            case "float": return 4;
+            case "int": return 4;
+            case "long": return 8;
+            case "double": return 8;
+            default: return 0;
+        }
+    }
+
+    public static int sizeOf(int val) { return getPrimitiveSizeByName(int.class.getName());}
+
+    public static int sizeOf(float val) {return getPrimitiveSizeByName(float.class.getName());}
+
+    public static int sizeOf(long val) {return  getPrimitiveSizeByName(long.class.getName());}
+
+    public static int sizeOf(double val) {return getPrimitiveSizeByName(double.class.getName());}
+
+    public static int sizeOf(byte val){ return getPrimitiveSizeByName(byte.class.getName());}
+
+    public static int sizeOf(boolean val){ return getPrimitiveSizeByName(boolean.class.getName());}
+
+    public static int sizeOf(short val){return getPrimitiveSizeByName(short.class.getName());}
+
+    public static int sizeOf(char val){return getPrimitiveSizeByName(char.class.getName());}
+
+    private static int getObjectSizeShift(int objectSize){
+        return 8 - (objectSize % 8);
+    }
+
+    private static int getArraySizeOf(int cellSize, int cellCount, BitJRE jre){
+        int object_size = jre.getHeaderSize() + 4 + cellSize * cellCount;
+        return object_size + getObjectSizeShift(object_size);
+    }
+
+    private static int primitiveArraySizeOf(Object val, BitJRE jre){
+        if(val instanceof boolean[]){
+            int primitive_size = sizeOf(getPrimitiveSizeByName(boolean.class.getName()));
+            return getArraySizeOf(primitive_size, ((boolean[])val).length, jre);
+        }
+
+        if(val instanceof byte[]){
+            int primitive_size = sizeOf(getPrimitiveSizeByName(byte.class.getName()));
+            return getArraySizeOf(primitive_size, ((byte[])val).length, jre);
+        }
+
+        if(val instanceof short[]){
+            int primitive_size = sizeOf(getPrimitiveSizeByName(short.class.getName()));
+            return getArraySizeOf(primitive_size, ((short[])val).length, jre);
+        }
+
+        if(val instanceof char[]){
+            int primitive_size = sizeOf(getPrimitiveSizeByName(char.class.getName()));
+            return getArraySizeOf(primitive_size, ((char[])val).length, jre);
+        }
+
+        if(val instanceof int[]){
+            int primitive_size = sizeOf(getPrimitiveSizeByName(int.class.getName()));
+            return getArraySizeOf(primitive_size, ((int[])val).length, jre);
+        }
+
+        if(val instanceof float[]){
+            int primitive_size = sizeOf(getPrimitiveSizeByName(float.class.getName()));
+            return getArraySizeOf(primitive_size, ((float[])val).length, jre);
+        }
+
+        if(val instanceof long[]){
+            int primitive_size = sizeOf(getPrimitiveSizeByName(long.class.getName()));
+            return getArraySizeOf(primitive_size, ((long[])val).length, jre);
+        }
+
+        if(val instanceof double[]){
+            int primitive_size = sizeOf(getPrimitiveSizeByName(double.class.getName()));
+            return getArraySizeOf(primitive_size, ((double[])val).length, jre);
+        }
+        return 0;
+    };
+
+    private static int objectArraySizeOf(Object val, BitJRE jre){
+        return getArraySizeOf(8, ((Object[]) val).length, jre);
+    }
+
+    private static Map<String, Integer> computeFieldSize(Object obj, BitJRE jre) throws Exception{
+        Map<String, Integer> result = new HashMap<>();
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            Class<?> fieldType = field.getType();
+            if(!Modifier.isStatic(field.getModifiers())){
+                if(fieldType.isPrimitive()){
+                    String name = "primitive:" + field.getName();
+                    Integer size = getPrimitiveSizeByName(fieldType.getName());
+                    result.put(name, size);
+                }else { //its reference
+                    if (fieldType.isArray()) {
+                        field.setAccessible(true);
+                        Object val = field.get(obj);
+                        if (val instanceof Object[]) {
+                            String name = "ref array:" + field.getName();
+                            Integer size = 4;
+                            result.put(name, size);
+                        } else {
+                            String name = "prim_array:" + field.getName();
+                            Integer size = primitiveArraySizeOf(val, jre);
+                            result.put(name, size);
+                        }
+                    }else {
+                        String name = "reference:" + field.getName();
+                        Integer size = 4;
+                        result.put(name, size);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static Map<String, Integer> detailedSizeOf(Object obj, BitJRE jre) throws Exception{
+        Map<String, Integer> detailInfo = new HashMap<>();
+        detailInfo.put("header", jre.getHeaderSize());
+        if (obj.getClass().isArray()) {
+            detailInfo.put("array_size_val", 4);
+        }else{
+//            detailInfo
+        }
+
+
+        Map<String, Integer> result = computeFieldSize(obj, jre);
+
+        int objSize = 0;
+        for (Integer val : result.values()) {
+            objSize += val;
+        }
+        int shiftSize = 8 - (objSize % 8);
+        result.put(shift_size_hdr, objSize);
+        result.put(total_size_hdr, objSize + shiftSize);
+        return result;
+    }
+
+    public static Map<String, Integer> detailedSizeOf(Object obj) throws Exception{
+        return detailedSizeOf(obj, curArch);
+    }
+
+    public static int sizeOf(Object obj, BitJRE jre) throws Exception{
+        return detailedSizeOf(obj, jre).get(total_size_hdr);
+    }
+
+    public static int sizeOf(Object obj) throws Exception{
+        return detailedSizeOf(obj).get(total_size_hdr);
+    }
+
+
+
+    public enum BitJRE{
+        x32(8), x64(16);
+
+        private int headerSize;
+        private BitJRE(int headerSize){
+            this.headerSize = headerSize;
+        }
+
+        public int getHeaderSize(){
+            return headerSize;
+        }
+    }
+}
---
+++ HW_02/src/test/java/by/dm13y/study/JavaObjectSizeTest.java
@@ -0,0 +1,48 @@
+package by.dm13y.study;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JavaObjectSizeTest {
+    class MyTest{
+        private Boolean[] BL = new Boolean[100];
+        private boolean[] bl = new boolean[100];
+    }
+
+    @Test
+    public void PrimitiveSizeTest() throws Exception{
+        assertTrue("check boolean size", ObjectSizeCalc.sizeOf(true)== 1);
+        assertTrue("check byte size", ObjectSizeCalc.sizeOf((byte)1)== 1);
+        assertTrue("check short size", ObjectSizeCalc.sizeOf((short)1)== 2);
+        assertTrue("check char size", ObjectSizeCalc.sizeOf('a')== 2);
+        assertTrue("check float size", ObjectSizeCalc.sizeOf(1.4f)== 4);
+        assertTrue("check int size", ObjectSizeCalc.sizeOf(10)== 4);
+        assertTrue("check long size", ObjectSizeCalc.sizeOf(10l)== 8);
+        assertTrue("check double size", ObjectSizeCalc.sizeOf(10.1)== 8);
+    }
+
+    @Test
+    public void PrimitiveByNameTest() throws Exception {
+        assertTrue("check boolean size", ObjectSizeCalc.getPrimitiveSizeByName(boolean.class.getName())== 1);
+        assertTrue("check byte size", ObjectSizeCalc.getPrimitiveSizeByName(byte.class.getName()) == 1);
+        assertTrue("check short size", ObjectSizeCalc.getPrimitiveSizeByName(char.class.getName()) ==2);
+        assertTrue("check char size", ObjectSizeCalc.getPrimitiveSizeByName(short.class.getName())== 2);
+        assertTrue("check float size", ObjectSizeCalc.getPrimitiveSizeByName(int.class.getName())== 4);
+        assertTrue("check int size", ObjectSizeCalc.getPrimitiveSizeByName(float.class.getName())== 4);
+        assertTrue("check long size", ObjectSizeCalc.getPrimitiveSizeByName(long.class.getName())== 8);
+        assertTrue("check double size", ObjectSizeCalc.getPrimitiveSizeByName(double.class.getName())== 8);
+    }
+
+    @Test
+    public void SimpleObjectSizeTest() throws Exception{
+//        assertTrue("check Integer size", ObjectSizeCalc.sizeOf(new Integer(1), ObjectSizeCalc.BitJRE.x32) == 16);
+//        assertTrue("check Integer size", ObjectSizeCalc.sizeOf(new Integer(1), ObjectSizeCalc.BitJRE.x64) == 24);
+        assertTrue("check boolean array size", ObjectSizeCalc.sizeOf(new MyTest(), ObjectSizeCalc.BitJRE.x32) == 4112);
+//        assertTrue("check boolean array size", ObjectSizeCalc.sizeOf(new boolean[1024], ObjectSizeCalc.BitJRE.x32) == 4112);
+
+        System.out.println(ObjectSizeCalc.sizeOf(new String(), ObjectSizeCalc.BitJRE.x32));
+        System.out.println(ObjectSizeCalc.detailedSizeOf(new String(), ObjectSizeCalc.BitJRE.x32));
+//        System.out.println(ObjectSizeCalc.computeFieldSize(new SimpleTestObject()));
+    }
+}
