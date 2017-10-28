9726e3a38554953cf63291c5ff313c1f5acefe33
dm13y <5132095@tut.by>
2017-10-28 10:41:40 +0300

    Simplified work of gc()

--- HW_02/src/main/java/by/dm13y/study/MemoryStandCalcGC.java
+++ HW_02/src/main/java/by/dm13y/study/MemoryStandCalcGC.java
@@ -1,9 +1,13 @@
 package by.dm13y.study;
 
+/**
+ * -XX:-UseCompressedOops -Xmx64m -Xms64m -XX:+PrintGCDetails
+ */
 public class MemoryStandCalcGC implements MemoryStand{
     private static Object obj;
     private static long objectSize;
     private static Runtime rt = Runtime.getRuntime();
+    private static final int SIZE = 100_000;
 
     public void setObject(Object obj) throws IllegalArgumentException {
         if(obj == null){
@@ -15,49 +19,33 @@ public class MemoryStandCalcGC implements MemoryStand{
         MemoryStandCalcGC.obj = obj;
     }
 
-    public Object makeObject(){
+    public Object makeObject() throws Exception{
         if(obj instanceof ObjectInitializer)
             return ((ObjectInitializer) obj).customInitObject();
         else {
-            return obj;
+            return obj.getClass().newInstance();
         }
     }
 
     @Override
     public void makeTest() throws Exception{
-        callGC();
-        final int arraySize = 10000;
-        long heapMemoryBeforeTest = 0;
-        Object[] objects = new Object[arraySize];
-        Object object = null;
-        callGC();
-        for (int i = -1; i < arraySize; i++) {
-            object = makeObject();
-            if(i < 0){
-                object = null;
-                callGC();
-                heapMemoryBeforeTest = busyMemory();
-            }else {
-                objects[i] = object;
-            }
+        final Object[] objects = new Object[SIZE];
+        rt.gc();
+        long memBeforeTest = busyMemory();
+
+        for (int i = 0; i < SIZE; i++) {
+            objects[i] = makeObject();
         }
-        callGC();
-        objectSize = (busyMemory() - heapMemoryBeforeTest) / arraySize;
-        for (Object object1 : objects) {
-            object1 = null;
+        rt.gc();
+        objectSize = (busyMemory() - memBeforeTest) / SIZE;
+        for (Object object : objects) {
+            object = null;
         }
     }
 
     @Override
     public long getResult() {
         return objectSize;
-    }
-
-    private static void callGC() throws Exception{
-        final int GC_countCalls = 20;
-        for (int i = 0; i < GC_countCalls; i++) {
-            rt.gc();
-        }
     }
 
     private static long busyMemory(){
