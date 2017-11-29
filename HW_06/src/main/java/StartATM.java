d0beac7bf39f949281e33d6c7c62aa03abb34e51
dm13y_work <Admin@mercom.by>
2017-11-29 16:51:40 +0300

    add ATM facade and starting class

---
+++ HW_06/src/main/java/StartATM.java
@@ -0,0 +1,73 @@
+import by.dm13y.study.atm.atm.ATMFacade;
+import by.dm13y.study.atm.atm.operations.Command;
+import by.dm13y.study.atm.bank.Bank;
+import by.dm13y.study.atm.cards.Card;
+import by.dm13y.study.atm.cards.CardReader;
+import by.dm13y.study.atm.cards.SimpleCardReader;
+import by.dm13y.study.atm.cashService.CashService;
+import by.dm13y.study.atm.cashService.SimpleCashService;
+import by.dm13y.study.atm.cashbox.CashBox;
+import by.dm13y.study.atm.cashbox.CashBoxImpl;
+import by.dm13y.study.atm.cashbox.CashCartridge;
+import by.dm13y.study.atm.cashbox.CashCartridgeImpl;
+import by.dm13y.study.atm.cashbox.picker.BanknotePicker;
+import by.dm13y.study.atm.cashbox.picker.BanknotePickerImpl;
+import by.dm13y.study.atm.diplay.Display;
+import by.dm13y.study.atm.money.Banknote;
+import by.dm13y.study.atm.pinpad.PinPadImpl;
+import by.dm13y.study.atm.pinpad.Pinpad;
+import by.dm13y.study.atm.printer.Printer;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StartATM {
+    private static ATMFacade atm;
+    private static CardReader cardReader;
+    private static CashService cashService;
+
+    public static void main(String[] ars){
+        Map<Banknote, CashCartridge> cashCartridgesBox = new HashMap<>();
+        cashCartridgesBox.put(Banknote.ONE_RUB, new CashCartridgeImpl(Banknote.ONE_RUB, 200, 100));
+        cashCartridgesBox.put(Banknote.TWO_RUB, new CashCartridgeImpl(Banknote.TWO_RUB, 200, 100));
+        cashCartridgesBox.put(Banknote.FIVE_RUB, new CashCartridgeImpl(Banknote.FIVE_RUB, 200, 100));
+        cashCartridgesBox.put(Banknote.TEN_RUB, new CashCartridgeImpl(Banknote.TEN_RUB, 200, 100));
+        cashCartridgesBox.put(Banknote.TWENTY_RUB, new CashCartridgeImpl(Banknote.TWENTY_RUB, 200, 100));
+
+        BanknotePicker bp = new BanknotePickerImpl();
+
+        CashBox cashBox = new CashBoxImpl(cashCartridgesBox, bp);
+
+        Bank bank = null;
+
+        Display display = new Display(){};
+
+        Pinpad pinpad = new PinPadImpl(System.in);
+
+        Printer printer = new Printer(){};
+
+        cardReader = new SimpleCardReader();
+
+        cashService = new SimpleCashService(Arrays.asList(Banknote.values()));
+
+        Map<Integer, Command > commands = Collections.EMPTY_MAP;
+
+        atm = new ATMFacade(bank, cashBox, display, pinpad, printer,
+                cardReader, cashService, commands);
+
+        Card card = new Card() {
+            @Override
+            public long getNumber() {
+                return 777;
+            }
+
+            @Override
+            public int getPin() {
+                return 1476;
+            }
+        };
+        ((SimpleCardReader) cardReader).processCard(card);
+    }
+}
---
+++ HW_06/src/main/java/by/dm13y/study/atm/atm/ATMFacade.java
@@ -0,0 +1,103 @@
+package by.dm13y.study.atm.atm;
+
+import by.dm13y.study.atm.atm.operations.Command;
+import by.dm13y.study.atm.bank.Bank;
+import by.dm13y.study.atm.cards.Card;
+import by.dm13y.study.atm.cards.CardReader;
+import by.dm13y.study.atm.cashService.CashService;
+import by.dm13y.study.atm.cashbox.CashBox;
+import by.dm13y.study.atm.diplay.Display;
+import by.dm13y.study.atm.money.Banknote;
+import by.dm13y.study.atm.pinpad.Pinpad;
+import by.dm13y.study.atm.printer.Printer;
+
+import java.util.*;
+
+public class ATMFacade implements Observer {
+    private final int COUNT_PIN_ATTEMPTS = 3;
+    private final Map<Integer, Command> commands;
+    private final List<String> displayListCommands;
+    private Bank bank;
+    private CardReader cardReader;
+    private CashService cashService;
+    private CashBox cashBox;
+    private Display display;
+    private Pinpad pinpad;
+    private Printer printer;
+
+    public ATMFacade(Bank bank, CashBox cashBox, Display display, Pinpad pinpad, Printer printer,
+                     CardReader cardReader, CashService cashService, Map<Integer, Command> commands) {
+        this.bank = bank;
+        this.cashBox = cashBox;
+        this.display = display;
+        this.pinpad = pinpad;
+        this.printer = printer;
+        this.cardReader = cardReader;
+        this.commands = commands;
+        this.cashService = cashService;
+
+        cardReader.addATMObserver(this);
+        cardReader.enable();
+
+        cashService.addATMObserver(this);
+
+        displayListCommands = new ArrayList<>();
+        commands.forEach((number, command) -> displayListCommands.add(number + "." + command.nameOperation()));
+
+        display.showHeader();
+    }
+
+    public void selectOperation(){
+        while (true) {
+            display.showList(displayListCommands);
+            try {
+                Integer commandCode = pinpad.enterValue();
+                Command cmd = commands.get(commandCode);
+                if (cmd != null) {
+                    cmd.exec();
+                }
+            } catch (Exception ex) {
+                display.showError("incorrect input, please again");
+            }
+        }
+    }
+
+    @Override
+    public void update(Observable observable, Object arg) {
+        if (arg == null) {return;}
+        if (cardReader == null) {return;}
+        if((observable instanceof CardReader) && (arg instanceof Card)){
+            display.showInfo("Thanks. Your card number is " +((Card) arg).getNumber() + "\n");
+            if (isCorrectPin(((Card) arg))) {
+                selectOperation();
+                ((CardReader) observable).disable();
+            }else {
+                display.showInfo("INPUT YOUR CARD\n");
+                ((CardReader) observable).returnCard();
+            }
+            return;
+        }
+
+        if((observable instanceof CashService) && (arg instanceof Banknote)){
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+    private boolean isCorrectPin(Card card) {
+        for (int i = 0; i < COUNT_PIN_ATTEMPTS; i++) {
+            display.getValue("Your pin");
+            try {
+                int pin = pinpad.enterValue();
+                if(pin != card.getPin()){
+                    throw new Exception();
+                }else {
+                    return true;
+                }
+            }catch (Exception ex){
+                display.showError("incorrect pin code, please try again");
+            }
+        }
+        return false;
+    }
+}
