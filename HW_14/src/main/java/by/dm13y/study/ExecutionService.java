0190f41083e644c924a680768f32dd3d207ff701
dm13y <5132095@tut.by>
2018-02-18 22:34:00 +0300

    HW_14 change lock to synchronize

--- HW_14/src/main/java/by/dm13y/study/ExecutionService.java
+++ HW_14/src/main/java/by/dm13y/study/ExecutionService.java
@@ -9,7 +9,6 @@ import java.util.concurrent.locks.ReentrantLock;
 class ExecutionService {
     private final ThreadPoolExecutor pool;
     private final int nThreads;
-    private final Lock lock = new ReentrantLock();
 
     public ExecutionService(int nThreads){
         pool = (ThreadPoolExecutor)Executors.newFixedThreadPool(nThreads);
@@ -18,15 +17,17 @@ class ExecutionService {
 
     public CompletableFuture<Void> execute(ThreadSortExecutor sortExecutor){
         CompletableFuture<Void> result = null;
-        lock.lock();
         try {
             if (!isBusy()) {
-                result = CompletableFuture.runAsync(sortExecutor, pool);
+                synchronized (this){
+                    if(!isBusy()){
+                        result = CompletableFuture.runAsync(sortExecutor, pool);
+                    }
+                }
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        lock.unlock();
         return result;
     }
 
